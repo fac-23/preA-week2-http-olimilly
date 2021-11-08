f109634c85eb82dade7548bca65baeb7df180e06
duckRabbitPy <duck.rabbit.python@gmail.com>
2021-11-08 21:06:31 +0000

    append pokemon after three correct answers
    Merged Quiz.js and Pokemon.js into App.js. Added a score variable which is incremented when answer is correct. Prize pokemon is fetched and appended on score 3, 6 and 9. Prize win sound added.

---
+++ app.js
@@ -0,0 +1,128 @@
+//set score to 0
+let score = 0;
+
+window.onload = sendApiRequest;
+
+// An asynchronous function to fetch data from the API
+async function sendApiRequest() {
+  let response = await fetch(
+    `https://opentdb.com/api.php?amount=1&type=multiple`
+  );
+  console.log(response);
+  let data = await response.json();
+  console.log(data);
+  useApiData(data);
+}
+
+// function to process the data received from the API
+function useApiData(data) {
+  document.querySelector(
+    "#category"
+  ).innerHTML = `Category: ${data.results[0].category}`;
+  document.querySelector(
+    "#difficulty"
+  ).innerHTML = `${data.results[0].difficulty}`;
+  document.querySelector(
+    "#question"
+  ).innerHTML = `Question: ${data.results[0].question}`;
+  document.querySelector("#answer1").innerHTML = data.results[0].correct_answer;
+  document.querySelector("#answer2").innerHTML =
+    data.results[0].incorrect_answers[0];
+  document.querySelector("#answer3").innerHTML =
+    data.results[0].incorrect_answers[1];
+  document.querySelector("#answer4").innerHTML =
+    data.results[0].incorrect_answers[2];
+}
+
+//function for correct answer
+
+let correctButton = document.querySelector("#answer1");
+
+correctButton.addEventListener("click", () => {
+  alert("You got it right!");
+  sendApiRequest();
+  //update score on correct answer
+  score++;
+  document.querySelector("#score").innerHTML = score;
+  if (score % 3 === 0) {
+    setPrize();
+  }
+});
+
+//funcion for incorrect answer
+
+let incorrectButton = document.querySelector("#answer2");
+
+incorrectButton.addEventListener("click", () => {
+  alert("Wrong Answer!");
+  sendApiRequest();
+});
+
+// -----------------------------------POKEMON API-------------------------------------------- //
+
+const prizeImage = document.getElementById("prizeImg");
+const prizeButton = document.getElementById("prizeBtn");
+const prizeSound = document.getElementById("prizeSound");
+
+//fetch array of objects that contain URL links to detailed individual pokemon objects
+async function collectPokemonArray() {
+  const pokemonArr = await fetch("https://pokeapi.co/api/v2/pokemon/");
+  return pokemonArr;
+}
+
+//fetch individual pokemon object from the API link
+async function collectPrize(url) {
+  const prize = await fetch(url);
+  return prize;
+}
+
+//extract pokemon PNG url from random pokemon object and update img src
+async function setPrizeUrl(collectPokemonArray, difficulty) {
+  collectPokemonArray()
+    .then((response) => {
+      response
+        .json()
+        .then((data) => data.results)
+        .then((data) => {
+          let randIndex = Math.floor(Math.random() * 20);
+          let pokemonURL = data[randIndex].url;
+          collectPrize(pokemonURL).then((response) => {
+            response.json().then((data) => {
+              switch (difficulty) {
+                case "easy":
+                  src = data.sprites.front_default;
+                  appendPrize(src);
+                  break;
+                case "medium":
+                  src = data.sprites.other[`official-artwork`].front_default;
+                  appendPrize(src);
+                  break;
+                case "hard":
+                  src = data.sprites.other.dream_world.front_default;
+                  appendPrize(src);
+                  break;
+              }
+            });
+          });
+        });
+    })
+    .catch((e) => {
+      console.log(
+        "There has been a problem with the fetch operation: " + e.message
+      );
+    });
+}
+
+function setPrize() {
+  prizeSound.play();
+  let difficulty = document.querySelector("#difficulty").innerHTML;
+  setPrizeUrl(collectPokemonArray, difficulty);
+}
+
+function appendPrize(src) {
+  let list = document.querySelector("#pokemonList");
+  var pokeImg = document.createElement("img");
+  pokeImg.setAttribute("id", "prizeImg");
+  pokeImg.setAttribute("src", src);
+  list.appendChild(pokeImg);
+}
