a9e6aae8da415d4e1e0ebd6be903fef82d2296a4
MillyP <millypalmer1@hotmail.co.uk>
2021-11-08 16:32:59 +0000

    Questions fetched, dom updated

--- quiz.js
+++ quiz.js
@@ -1,17 +1,40 @@
-    /* Using API URL generator on Open Trivia DataBase website, it fetched 25 questions,
-    with a combination of easy, medium and difficult levels, for all categories,
-    and has shown up in Google Chrome as retrieved JSON in the browser console */
-
-
-    const getQuestions = fetch("https://opentdb.com/api.php?amount=25")
-    .then((response) => response.json())
-    .then((data) => console.log(data)); 
-    
-    const level = document.querySelector(".level");
-
-    function click() {
-
-    level.addEventListener('click', (e) => {
-      e.getQuestions();
-    });
+  window.onload = sendApiRequest
+  
+  // An asynchronous function to fetch data from the API
+  async function sendApiRequest() {
+    let response = await fetch(`https://opentdb.com/api.php?amount=1&type=multiple`);
+    console.log(response)
+    let data = await response.json()
+    console.log(data)
+    useApiData(data)
   }
+  
+  // function to process the data received from the API
+  function useApiData (data) {
+  document.querySelector("#category").innerHTML = `Category: ${data.results[0].category}`
+  document.querySelector("#difficulty").innerHTML = `Difficulty: ${data.results[0].difficulty}`
+  document.querySelector("#question").innerHTML = `Question: ${data.results[0].question}`
+  document.querySelector("#answer1").innerHTML = data.results[0].correct_answer
+  document.querySelector("#answer2").innerHTML = data.results[0].incorrect_answers[0]
+  document.querySelector("#answer3").innerHTML = data.results[0].incorrect_answers[1]
+  document.querySelector("#answer4").innerHTML = data.results[0].incorrect_answers[2]
+  }
+
+  //function for correct answer
+
+  let correctButton = document.querySelector("#answer1")
+
+  correctButton.addEventListener("click", () => {
+    alert("You got it right!")
+    sendApiRequest()
+  })
+
+  //funcion for incorrect answer
+  
+  let incorrectButton = document.querySelector("#answer2")
+
+  incorrectButton.addEventListener("click", () => {
+    alert("Wrong Answer!")
+    sendApiRequest()
+  })
+  
